d417b50ae883d92a52428834596c86ce756d0870
André Felipe de Lima Veloso <andreveloso.477@gmail.com>
2025-09-03 17:47:41 -0300

    Atividade 02 Orientação á Objetos
    Atividade 02 Orientação á Objetos

---
+++ Atividade 02 Orientação á objetos/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+    EXERCÍCIO 02
+
+    - Criar uma classe Aluno, contendo os atributos:
+        a) nome
+        b) nota1
+        c) nota2
+
+    Haverá três métodos, sendo dois privados e um público:
+    Media: Método privado para retornar a média do aluno.
+    Situação: Método privado para retornar se está aprovado ou reprovado.
+    Mensagem: Método publico para retornar o nome, média e situação.
+
+    - Vamos utilizar: Classe, atributo, método, objeto e encapsulamento.    
+*/
+
+// Criar objeto
+Aluno obj = new();
+obj.Nome = "Ralf";
+obj.Nota1 = 8;
+obj.Nota2 = 10;
+obj.Mensagem();
